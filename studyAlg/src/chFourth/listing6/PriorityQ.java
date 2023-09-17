5a793bea7aad8dd4ff4270f5ecf2021fca1c1066
YaroslavBalsa <balsayaroslav@gmail.com>
2023-09-17 15:50:00 +0300

    chapter 4, listing 6

---
+++ studyAlg/src/chFourth/listing6/PriorityQ.java
@@ -0,0 +1,43 @@
+package chFourth.listing6;
+
+public class PriorityQ {
+    private long [] priorityQArray;
+    private int nElem;
+    private int maxSize;
+
+    public PriorityQ(int maxSize) {
+        this.maxSize = maxSize;
+        priorityQArray = new long[maxSize];
+        nElem = 0;
+    }
+    public void insert (long element){
+
+        int i;
+
+        if (nElem == 0){
+            priorityQArray[nElem++] = element;
+        } else {
+            for (i = nElem - 1; i >= 0; i--){
+                if (element > priorityQArray[i]){
+                    priorityQArray[i + 1] = priorityQArray[i];
+                } else {
+                    break;
+                }
+            }
+            priorityQArray[i + 1] = element;
+            nElem++;
+        }
+    }
+    public long remove (){
+        return priorityQArray[--nElem];
+    }
+    public long peekMIn (){
+        return priorityQArray[nElem - 1];
+    }
+    public boolean isEmpty (){
+        return (nElem == 0);
+    }
+    public boolean isFull (){
+        return (nElem == maxSize);
+    }
+}
---
+++ studyAlg/src/chFourth/listing6/PriorityQApp.java
@@ -0,0 +1,19 @@
+package chFourth.listing6;
+
+public class PriorityQApp {
+    public static void main(String[] args) {
+        PriorityQ queue = new PriorityQ(5);
+
+        queue.insert(30);
+        queue.insert(50);
+        queue.insert(10);
+        queue.insert(40);
+        queue.insert(20);
+
+        while (!queue.isEmpty()){
+            long element = queue.remove();
+            System.out.println(element + " ");
+        }
+        System.out.println("");
+    }
+}
