a17de95dbb2b6ce2c312e65040aa7cea05c818dd
YaroslavBalsa <138520591+YaroslavBalsa@users.noreply.github.com>
2024-01-20 13:05:17 +0300

    chapter 5, listing 2

--- studyAlg/src/chFifth/listing1/LinkList.java
+++ studyAlg/src/chFifth/listing1/LinkList.java
@@ -3,14 +3,47 @@ package chFifth.listing1;
 public class LinkList {
     private Link first;
 
-    public boolean isEmpty() {
-        return (first == null);
+    public LinkList() {
+        this.first = null;
     }
+
     public void insertFirst(int iDate, double dDate) {
         Link newLink = new Link(iDate, dDate);
+        newLink.setiDate(iDate);
+        newLink.setdData(dDate);
         newLink.next = first;
 
         first = newLink;
+    }
+    public Link find(int key) {
+        Link current = first;
+
+        while (current.getiDate() != key) {
+            if (current.next == null) {
+                return null;
+            } else {
+                current = current.next;
+            }
+        }
+        return current;
+    }
+    public Link delete(int key) {
+        Link current = first;
+        Link previous = first;
+        while (current.getiDate() != key) {
+            if (current.next == null) {
+                return null;
+            } else {
+                previous = current;
+                current = current.next;
+            }
+        }
+        if (current == first) {
+            first = first.next;
+        } else {
+            previous.next = current.next;
+        }
+        return current;
     }
     public Link deleteFirst() {
         Link temp = null;
@@ -19,6 +52,9 @@ public class LinkList {
             first = first.next;
         }
         return temp;
+    }
+    public boolean isEmpty() {
+        return (first == null);
     }
     public void displayList() {
         System.out.print("List (first --> last): ");
--- studyAlg/src/chFifth/listing1/LinkListApp.java
+++ studyAlg/src/chFifth/listing1/LinkListApp.java
@@ -2,6 +2,7 @@ package chFifth.listing1;
 
 public class LinkListApp {
     public static void main(String[] args) {
+
         LinkList theList = new LinkList();
         theList.insertFirst(22, 2.99);
         theList.insertFirst(44, 4.99);
@@ -9,6 +10,20 @@ public class LinkListApp {
         theList.insertFirst(88, 8.99);
 
         theList.displayList();
+
+        Link f = theList.find(44);
+        if (f != null) {
+            System.out.println("Найден элемент c ключём " + f.getiDate());
+        } else {
+            System.out.println("Данный элемент не найден");
+        }
+
+        Link d = theList.delete(66);
+        if (d != null) {
+            System.out.println("Удален элемент с ключём " + d.getiDate());
+        } else {
+            System.out.println("Данный элемент не найден");
+        }
 
         while(!theList.isEmpty()) {
             Link link = theList.deleteFirst();
